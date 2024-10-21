f59c939f642882e88a50e8f3242df5cc79a7c1a8
crynext141 <tarasfurtak305@gmail.com>
2024-10-21 14:48:13 +0300

    Implemented API Key authentication

---
+++ CodeVeronicaALRS/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,44 @@
+﻿
+namespace CodeVeronicaALRS.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string ApiKeyHeaderName = "X-API-KEY";
+
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IConfiguration configuration)
+        {
+         
+            if (context.Request.Path.StartsWithSegments("/swagger"))
+            {
+                await _next(context);
+                return;
+            }
+
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var providedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key is missing.");
+                return;
+            }
+
+            var apiKeyFromConfig = configuration["ApiKey"];
+
+            if (!apiKeyFromConfig.Equals(providedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+
+}
